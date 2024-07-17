3a73ec7222bd3ab2ee303b201b737fe4e6da8749
Ho Trung <khanhtrung14111999@gmail.com>
2024-07-17 17:28:15 +0300

    HomePage and PatientForm. Using Form in shadcn/ui with React Hook Form and zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/*add otp  verification here maybe and with passkey modal*/}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -13,8 +16,27 @@ export default function Home() {
             alt='patient'
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justtify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+
+            <Link href='/?admin=true' className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <Image
+        src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
