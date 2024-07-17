cfaae8fc13b99daf4c5c895a159afef81cf63ebc
Ho Trung <khanhtrung14111999@gmail.com>
2024-07-17 19:19:50 +0300

    Homepage done, prepare for catching userData

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -21,6 +22,7 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
+    const router = useRouter()
     const [isLoading, setIsLoading] = useState(false)
     const form = useForm<z.infer<typeof UserFormValidation>>({
         resolver: zodResolver(UserFormValidation),
@@ -31,8 +33,19 @@ const PatientForm = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof UserFormValidation>) {
-        console.log(values)
+    function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
+
+        try {
+            // const userData = { name, email, phone };
+
+            // const user = await createUser(userData);
+
+            // if(user) router.push(`/patients/${user.$id}/register`)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
     return (
         <div>
