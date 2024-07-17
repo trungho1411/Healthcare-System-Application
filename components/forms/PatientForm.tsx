9f0be33d7add507c1d53c9c443c9a451149ecb12
Ho Trung <khanhtrung14111999@gmail.com>
2024-07-17 23:06:25 +0300

    Add user after installing setup with appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,13 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
     INPUT = 'input',
@@ -37,13 +37,13 @@ const PatientForm = () => {
         setIsLoading(true)
 
         try {
-            // const userData = { name, email, phone };
+            const userData = { name, email, phone };
 
-            // const user = await createUser(userData);
+            const user = await createUser(userData);
 
-            // if(user) router.push(`/patients/${user.$id}/register`)
+            if (user) router.push(`/patients/${user.$id}/register`)
 
-        } catch (error) {
+        } catch (error: any) {
             console.log(error)
         }
     }
