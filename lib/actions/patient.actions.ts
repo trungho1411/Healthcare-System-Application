ad765047fafa0be47ab8ed7de8f7b6aa85f4c358
Ho Trung <khanhtrung14111999@gmail.com>
2024-07-22 20:37:38 +0300

    server for new appointment page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,11 +1,21 @@
-import { ID, Models, Query } from 'node-appwrite';
-import { users } from '../appwrite.config';
+'use server';
+
+import { ID, Query } from 'node-appwrite';
+import {
+  BUCKET_ID,
+  ENDPOINT,
+  PROJECT_ID,
+  storage,
+  users,
+} from '../appwrite.config';
 import { parseStringify } from '../utils';
 import {
   DATABASE_ID,
   PATIENT_COLLECTION_ID,
   databases,
 } from '../appwrite.config';
+
+import { InputFile } from 'node-appwrite/file';
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -18,7 +28,6 @@ export const createUser = async (user: CreateUserParams) => {
     );
     console.log({ newUser });
     return parseStringify(newUser);
-
   } catch (error: any) {
     if (error && error?.code === 409) {
       const existingUser = await users.list([
@@ -59,5 +68,35 @@ export const getPatient = async (userId: string) => {
       'An error occurred while retrieving the patient details:',
       error
     );
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string
+      );
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view? project=${PROJECT_ID}`,
+        ...patient,
+      }
+    );
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.log(error);
   }
 };
